be84a4654f0902102af382f7d9d42f18fb1573e3
Anders Kvellestad <anders.kvellestad@fys.uio.no>
2025-08-22 08:58:13 +0200

    Added Hello World example to the code examples.

---
+++ code_examples/hello_world/main.cpp
@@ -0,0 +1,12 @@
+// Look, our first C++ program!
+
+#include <iostream>
+
+int main()
+{
+  // Print a message to screen
+  std::cout << "Hello, World!\n";
+
+  // Return 0 when done
+  return 0;
+}
